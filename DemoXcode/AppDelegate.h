237921b00bc7ad0d5b2489efb93ff0a83b85c540
Pankaj Phartiyal <pankajphartiyal90@gmail.com>
2014-11-19 12:01:05 +0530

    Initial Commit

---
+++ DemoXcode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoXcode
+//
+//  Created by Pankaj on 19/11/14.
+//  Copyright (c) 2014 Mobilemerit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoXcode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoXcode
+//
+//  Created by Pankaj on 19/11/14.
+//  Copyright (c) 2014 Mobilemerit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
